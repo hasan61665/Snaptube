7451e9324f37e83403570799647f24ae701591ef
hasan61665 <darkguardian00065@gmail.com>
2025-03-09 06:34:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll("main section");
+    sections.forEach(section => {
+        section.style.display = "none";
+    });
+    document.getElementById(sectionId).style.display = "block";
+}
